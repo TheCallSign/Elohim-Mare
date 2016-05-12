70c051a3e6a1bf641882af54fb7234c3d8e827a2
St John Giddy <stjohn@thecallsign.net>
2016-05-12 14:10:35 +0200

    Add project files.

---
+++ Elohim Mar'e/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elohim_Mar_e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
